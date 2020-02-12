e4cfa8db666f4f75a9c33af1082f24daca6483e7
safarudin <safarudinyoga@gmail.com>
2020-02-12 15:47:44 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled>Correct Answer</b-nav-item>
+      <b-nav-item disabled>0/10</b-nav-item>
+      <b-nav-item disabled>Reset</b-nav-item>
+    </b-nav>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
 import App from './App.vue'
+
+// app.js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
